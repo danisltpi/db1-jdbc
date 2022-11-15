bcdaa578c9748ad66ccd67a66adc178264466deb
Maik Grunicke <h.m.grunicke@gmail.com>
2022-11-15 11:04:39 +0100

    Refactor code into separate method

--- src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
+++ src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
@@ -5,6 +5,10 @@ import java.util.Properties;
 
 public class DatabaseConnection { 
   public static void main(String args[]) throws ClassNotFoundException {
+    Connection conn = establishConnection();
+
+  }
+  public static Connection establishConnection(){
     String url = "jdbc:postgresql://datenbanken1.ddns.net:3690/";
     Properties props = new Properties();
     props.setProperty("user", "g22");
@@ -12,8 +16,10 @@ public class DatabaseConnection {
 
     try {
       Connection conn = DriverManager.getConnection(url, props);
+      return conn;
     } catch (SQLException e) {
       e.printStackTrace();
+      return null;
     }
   }
 }
