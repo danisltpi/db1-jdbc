d8aa4ede22562c9faa45b48e9469def6ef53018d
danisltpi <lcdns@protonmail.com>
2022-11-15 21:50:45 +0100

    format

--- src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
+++ src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
@@ -3,14 +3,14 @@ package de.hska.iwii.db1.jdbc;
 import java.sql.*;
 import java.util.Properties;
 
-public class DatabaseConnection { 
+public class DatabaseConnection {
   public static void main(String args[]) throws ClassNotFoundException {
     Connection conn = establishConnection();
     getInfo(conn);
     disconnectDatabase(conn);
   }
 
-  public static Connection establishConnection(){
+  public static Connection establishConnection() {
     String url = "jdbc:postgresql://datenbanken1.ddns.net:3690/";
     Properties props = new Properties();
     props.setProperty("user", "g22");
@@ -25,23 +25,23 @@ public class DatabaseConnection {
     }
   }
 
-  public static void getInfo(Connection conn){
-    try{
+  public static void getInfo(Connection conn) {
+    try {
       DatabaseMetaData metaData = conn.getMetaData();
       String dbName = metaData.getDatabaseProductName();
       String dbDriver = metaData.getDriverName();
 
       String information = String.format("Database Name: %s\nDriver: %s", dbName, dbDriver);
       System.out.println(information);
-    }catch(Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  public static void disconnectDatabase(Connection conn){
-    try{
+  public static void disconnectDatabase(Connection conn) {
+    try {
       conn.close();
-    }catch(Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
