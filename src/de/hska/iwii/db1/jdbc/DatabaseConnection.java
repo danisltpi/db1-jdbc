2e27354bdf1e93adae4d129e9c7fe3205b958226
Maik Grunicke <h.m.grunicke@gmail.com>
2022-11-15 13:15:58 +0100

    feature: Add solution for 4.1 and resolved password error

--- src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
+++ src/de/hska/iwii/db1/jdbc/DatabaseConnection.java
@@ -6,20 +6,43 @@ import java.util.Properties;
 public class DatabaseConnection { 
   public static void main(String args[]) throws ClassNotFoundException {
     Connection conn = establishConnection();
-
+    getInfo(conn);
+    disconnectDatabase(conn);
   }
+
   public static Connection establishConnection(){
     String url = "jdbc:postgresql://datenbanken1.ddns.net:3690/";
     Properties props = new Properties();
     props.setProperty("user", "g22");
-    props.setProperty("password", "agmbQckaKCj");
+    props.setProperty("password", "agmbQckaKC");
 
     try {
       Connection conn = DriverManager.getConnection(url, props);
       return conn;
-    } catch (SQLException e) {
+    } catch (Exception e) {
       e.printStackTrace();
       return null;
+    }
+  }
+
+  public static void getInfo(Connection conn){
+    try{
+      DatabaseMetaData metaData = conn.getMetaData();
+      String dbName = metaData.getDatabaseProductName();
+      String dbDriver = metaData.getDriverName();
+
+      String information = String.format("Database Name: %s\nDriver: %s", dbName, dbDriver);
+      System.out.println(information);
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void disconnectDatabase(Connection conn){
+    try{
+      conn.close();
+    }catch(Exception e){
+      e.printStackTrace();
     }
   }
 }
